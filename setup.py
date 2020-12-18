24b660a18cb17ad04f3dbb2e581045efcad37e26
nielsvaes-es <niels.vaes@embark-studios.com>
2020-12-18 13:40:11 +0100

    making some distinction between Python 2 and 3 for the required imports

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+import sys
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+if sys.version.major == 2:
+    requires = ['requests', 'pyside', 'websockets']
+else:
+    requires = ['requests', 'pyside2', 'websockets']
 
 setuptools.setup(
     name="skyhook",
@@ -12,7 +18,7 @@ setuptools.setup(
     long_description="Engine and DCC communication system",
     long_description_content_type="text/markdown",
     url="https://github.com/EmbarkStudios/skyhook",
-    install_requires=['requests', 'pyside2', 'websockets'],
+    install_requires=requires,
     packages=setuptools.find_packages(),
     classifiers=[
         "Operating System :: OS Independent",
