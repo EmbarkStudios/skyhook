9011e3279fa4666970ce5dd376cd40d350170923
nisse <nielsvaes@gmail.com>
2020-12-21 10:47:20 +0100

    separated out print_to_screen

--- skyhook/logger.py
+++ skyhook/logger.py
@@ -16,29 +16,29 @@ class Logger(object):
         self.file_path = file_path if file_path is not None else os.path.join(os.path.expanduser("~"), "skyhook.log")
         self.__print_to_screen = print_to_screen
 
-        self.debug("\n\n====================== NEW LOG ======================")
+        self.debug("\n\n====================== NEW LOG ======================", print_to_screen=False)
 
-    def success(self, message):
-        self.__add_to_log(Status.SUCCESS, message)
+    def success(self, message, print_to_screen=True):
+        self.__add_to_log(Status.SUCCESS, message, print_to_screen)
 
-    def warning(self, message):
-        self.__add_to_log(Status.WARNING, message)
+    def warning(self, message, print_to_screen=True):
+        self.__add_to_log(Status.WARNING, message, print_to_screen)
 
-    def info(self, message):
-        self.__add_to_log(Status.INFO, message)
+    def info(self, message, print_to_screen=True):
+        self.__add_to_log(Status.INFO, message, print_to_screen)
 
-    def error(self, message):
-        self.__add_to_log(Status.ERROR, message)
+    def error(self, message, print_to_screen=True):
+        self.__add_to_log(Status.ERROR, message, print_to_screen)
 
-    def debug(self, message):
-        self.__add_to_log(Status.DEBUG, message)
+    def debug(self, message, print_to_screen=True):
+        self.__add_to_log(Status.DEBUG, message, print_to_screen)
 
-    def __add_to_log(self, status, message):
+    def __add_to_log(self, status, message, print_to_screen):
         time = datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
         line = "[%s] | %s | %s" % (time, status, message)
 
         with open(self.file_path, "a") as out_file:
             out_file.write(line + "\n")
 
-        if self.__print_to_screen:
+        if print_to_screen:
             print(line)
