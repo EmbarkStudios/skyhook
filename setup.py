629d45cfdf2abc3bd1e02a099f94563ca2c9a95a
Niels Vaes <niels.vaes@embark-studios.com>
2022-03-18 14:24:37 +0100

    version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setuptools.setup(
     name="skyhook",
-    version="2.2.0",
+    version="2.2.1",
     author="Niels Vaes",
     author_email="niels.vaes@embark-studios.com",
     description="Engine and DCC communication system",
